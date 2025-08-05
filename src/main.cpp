e220acbb9c7387e7b9cf607ceece1ffe8fc58dc8
Matin22 <gholami.matin@gmail.com>
2025-08-05 22:12:53 +0200

    added texture and vertexarray

--- src/main.cpp
+++ src/main.cpp
@@ -46,12 +46,69 @@ std::vector<Star> createStars(uint32_t count, float scale)
     return stars;
 }
 
+void updateGeometry(uint32_t idx, Star const& s, sf::VertexArray& va)
+{
+    float const scale = 1.0f / s.z;
+    float const depth_ratio = (s.z - conf::near) / (conf::far - conf::near);
+    float const color_ratio = 1.0f - depth_ratio;
+    auto const c = static_cast<uint8_t>(color_ratio * 255.0f);
+    sf::Color color(c, c, c);
+
+    sf::Vector2f const p = s.position * scale;
+    float const r = conf::radius * scale;
+    uint32_t const i = 6 * idx;
+    
+    va[i + 0].position = sf::Vector2f(p.x - r, p.y - r);
+    va[i + 1].position = sf::Vector2f(p.x + r, p.y - r);
+    va[i + 2].position = sf::Vector2f(p.x - r, p.y + r);
+    va[i + 3].position = sf::Vector2f(p.x + r, p.y - r);
+    va[i + 4].position = sf::Vector2f(p.x + r, p.y + r);
+    va[i + 5].position = sf::Vector2f(p.x - r, p.y + r);
+
+    va[i + 0].color = color;
+    va[i + 1].color = color;
+    va[i + 2].color = color;
+    va[i + 3].color = color;
+    va[i + 4].color = color;
+    va[i + 5].color = color;
+}
+
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({conf::WINDOW_SIZE.x, conf::WINDOW_SIZE.y}), "CMake SFML Project", conf::window_state);
     window.setFramerateLimit(conf::MAX_FRAMERATE);
+    // window.setMouseCursorVisible(false);
+
+    sf::Texture texture;
+    texture.setSmooth(true);
+    if (!texture.loadFromFile("res/star.png"))
+    {
+        std::cerr << "Failed to load star.png" << std::endl;
+        return -1;
+    }
+    if (!texture.generateMipmap())
+    {
+        std::cerr << "Failed to generate mipmap" << std::endl;
+    }
 
     std::vector<Star> stars = createStars(conf::count, conf::far);
+
+    sf::VertexArray va(sf::PrimitiveType::Triangles, 6 * conf::count);
+
+    // prefill texture coords -> constant
+    auto const texture_size_f = static_cast<sf::Vector2f>(texture.getSize());
+    for (uint32_t idx{conf::count}; idx--;)
+    {
+        uint32_t const i = 6 * idx;
+        
+        va[i + 0].texCoords = sf::Vector2f(0, 0);
+        va[i + 1].texCoords = sf::Vector2f(texture_size_f.x, 0);
+        va[i + 2].texCoords = sf::Vector2f(0, texture_size_f.y);
+
+        va[i + 3].texCoords = sf::Vector2f(texture_size_f.x, 0);
+        va[i + 4].texCoords = sf::Vector2f(texture_size_f.x, texture_size_f.y);
+        va[i + 5].texCoords = sf::Vector2f(0, texture_size_f.y);
+    }
 
     while (window.isOpen())
     {
@@ -73,26 +130,19 @@ int main()
         }
 
         window.clear();
-        // rendering
-        sf::CircleShape shape{conf::radius};
-        shape.setOrigin(sf::Vector2f{conf::radius, conf::radius});
+
         for (uint32_t i{conf::count}; i--;)
         {
             uint32_t idx = (i + first) % conf::count;
             Star const &s = stars[idx];
-            
-            float const scale = 1.0f / s.z;
-            shape.setPosition(s.position * scale + conf::fWINDOW_SIZE * 0.5f);
-            shape.setScale(sf::Vector2f{scale, scale});
-            
-            float const depth_ratio = (s.z - conf::near) / (conf::far - conf::near);
-            float const color_ratio = 1.0f - depth_ratio;
-            auto const c = static_cast<uint8_t>(color_ratio * 255.0f);
-            shape.setFillColor({c, c, c});
-            
-            window.draw(shape);
+            updateGeometry(i, s, va);
         }
-        
+
+        sf::RenderStates states;
+        states.transform.translate(conf::fWINDOW_SIZE * 0.5f);
+        states.texture = &texture;
+        window.draw(va, states);
+
         window.display();
     }
 }
